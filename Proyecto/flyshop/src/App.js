69b1bcbdf37fcf2482d769cba65eabdb5dffbacc
mandrakhe <luisfernnandomb@gmail.com>
2024-02-07 18:31:41 -0500

    Inicio del proyecto

---
+++ Proyecto/flyshop/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './nav'
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ Proyecto/flyshop/src/nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+  <>
+  
+  </>
+  )
+}
+
+export default nav
