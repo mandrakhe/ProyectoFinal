9efb0943d66223c1ba93f1471af4ef3234e68b82
Faberfloo21 <andresfaber8@gmail.com>
2024-02-19 13:33:12 -0500

    Actualización

--- flyshop/src/footer.js
+++ flyshop/src/footer.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { FaFacebookF } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io";
-import { FaXTwitter } from "react-icons/fa6";
-import { RiTiktokLine } from "react-icons/ri";
-import Logo from './images/logos/logo_flyshop.png'
+import { FaWhatsapp } from "react-icons/fa6";
+import Logo from './images/logos/logo_flyshop.png';
 import './footer.css'
 const Footer = () => {
     return (
@@ -14,35 +12,30 @@ const Footer = () => {
                         <div className='logo'>
                             <img src={Logo} alt='logo'/>
                         </div>
-                        <div className='detail'>
-                            <p>Información relacionada a flyshop y ese poco de vainas</p>
-                            <div className='icon'>
-                                <li><FaFacebookF /></li>
-                                <li><IoLogoInstagram /></li>
-                                <li><FaXTwitter /></li>
-                                <li><RiTiktokLine /></li>
-                            </div>
-                        </div>
                     </div>
                     <div className='account'>
-                        <h3>My Account</h3>
+                        <h3>Cuenta</h3>
                         <ul>
-                            <li>Account</li>
-                            <li>Order</li>
-                            <li>Cart</li>
-                            <li>Shipping</li>
-                            <li>Return</li>
+                            <li>Pedidos</li>
+                            <li>Favoritos</li>
+                            <li>Mi cuenta</li>
+                            <li>Carrito de compras</li>
                         </ul>
                     </div>
                     <div className='page'>
-                        <h3>Pages</h3>
+                        <h3>Pagina</h3>
                         <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                            <li>Term & Condition</li>
+                            <li>Inicio</li>
+                            <li>Tienda</li>
+                            <li>Contáctanos</li>
                         </ul>
                     </div>
+                        <div className='detail'>
+                            <div className='icon'>
+                                <li><IoLogoInstagram /></li>
+                                <li><FaWhatsapp /></li>
+                            </div>
+                        </div>
                 </div>
             </div>
         </>
