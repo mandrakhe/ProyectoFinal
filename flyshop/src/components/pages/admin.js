f8c45e42fa81b54aa45df00fdc67c32be2f558a1
mandrakhe <111034550+mandrakhe@users.noreply.github.com>
2024-03-11 02:02:35 -0500

    diseño addproduct

--- flyshop/src/components/pages/admin.js
+++ flyshop/src/components/pages/admin.js
@@ -8,35 +8,7 @@ function Admin() {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-
-          <div className="col-md-4" >
-            <div className="card mb-4">
-              <div className="card-body">
-                <h5 className="card-title"></h5>
-                <p className="card-text"></p>
-                <p className="card-text">
-                  <strong>Price:</strong>
-                </p>
-                <button
-                  className="btn btn-primary"
-
-                >
-                  Edit
-                </button>
-                <button
-                  className="btn btn-danger"
-
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
+     
     </>
   )
 }
